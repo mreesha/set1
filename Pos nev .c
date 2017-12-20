4b7528d98c4e1eac4488391ab170fb1c54bb6ac8
mreesha <34651373+mreesha@users.noreply.github.com>
2017-12-20 22:48:06 +0530

    Create Pos nev .c

---
+++ Pos nev .c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+Int main()
+{
+int n;
+Cin>>n;
+if(n>0=positive && n<0=negative && n=zero)
+{
+Cout<<"positive";
+}
+Else if
+{
+Cout<<"negative";
+}
+Else if
+{
+Cout<<"zero";
+}
+Return 0;
+}
